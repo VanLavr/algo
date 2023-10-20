6d572d5e36821f2f641cf94dab0652b95098642c
VanLavr <lavr.i-n@mail.ru>
2023-10-20 21:24:09 +0300

    merge sort added

--- merge_sort/merge_sort.go
+++ merge_sort/merge_sort.go
@@ -1,66 +1,67 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-    var a = []int{1, 3, 2, 5, 3, 4, 345, 234, 5, 23}
-    b := MergeSort(a)
-    fmt.Println(b)
+	var a = []int{1234, 345, 4, 234, 5, 9090, 23, 8721}
+	fmt.Println(a)
+	b := MergeSort(a)
+	fmt.Println(b)
 }
 
 func MergeSort(array []int) []int {
-    middle := len(array) / 2
-    left := make([]int, middle)
-    right := make([]int, middle + len(array) % 2)
+	if len(array) < 2 {
+		return array
+	}
+	middle := len(array) / 2
+	left := make([]int, middle)
+	right := make([]int, middle+len(array)%2)
 
-    for index, value := range array {
-        if index < len(array) / 2 {
-            left[index] = value
-        } else {
-            right[index] = value
-        }
-    }
+	iter := 0
+	for index, value := range array {
+		if index < len(array)/2 {
+			left[index] = value
+		} else {
+			right[iter] = value
+			iter++
+		}
+	}
 
-    MergeSort(left)
-    MergeSort(right)
-    result := Merge(array, left, right)
-    return result
+	left = MergeSort(left)
+	right = MergeSort(right)
+	result := Merge(array, left, right)
+	return result
 }
 
 func Merge(origin, l_array, r_array []int) []int {
-    merged := make([]int, len(l_array) + len(r_array))
+	i := 0
+	j := 0
+	iterator := 0
+	for i < len(l_array) && j < len(r_array) {
+		if l_array[i] < r_array[j] {
+			origin[iterator] = l_array[i]
+			i++
+			iterator++
+		} else {
+			origin[iterator] = r_array[j]
+			j++
+			iterator++
+		}
+	}
 
-    i := 0
-    j := 0
-    iterator := 0
-    for i < len(l_array) && j < len(r_array) {
-        if l_array[i] < r_array[j] {
-            origin[iterator] = l_array[i]
-            i++
-            iterator++
-        } else {
-            origin[iterator] = r_array[j]
-            j++
-            iterator++
-        }
-    }
+	for i < len(l_array) {
+		origin[iterator] = l_array[i]
+		i++
+		iterator++
+	}
 
-    for i < len(l_array) {
-        origin[iterator] = l_array[i]
-    }
+	for j < len(r_array) {
+		origin[iterator] = r_array[j]
+		j++
+		iterator++
+	}
 
-    for j < len(r_array) {
-        origin[iterator] = r_array[j]
-    }
-    
-    return origin
+	return origin
 }
-
-
-
-
-
-
-
